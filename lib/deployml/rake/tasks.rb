0ae5a3ec1e9681fb20e31e4f9ec64a1c7425d42e
postmodern <postmodern.mod3@gmail.com>
2010-03-10 02:33:51 -0800

    Added descriptions to the rake tasks.

--- lib/deployml/rake/tasks.rb
+++ lib/deployml/rake/tasks.rb
@@ -5,14 +5,17 @@ namespace :deploy do
     @project = DeploYML::Project.new(Dir.pwd)
   end
 
+  desc 'Downloads the project'
   task :download => :project do
     @project.download!
   end
 
+  desc 'Uploads the project'
   task :upload => :project do
     @project.upload!
   end
 
+  desc 'Deploys the project'
   task :deploy => :project do
     @project.deploy!
   end

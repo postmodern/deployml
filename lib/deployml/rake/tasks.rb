2fe019e11933df89c40a6502a89ae6199f6881cb
postmodern <postmodern.mod3@gmail.com>
2010-03-10 02:41:21 -0800

    Added diagnostic messages to the rake tasks.

--- lib/deployml/rake/tasks.rb
+++ lib/deployml/rake/tasks.rb
@@ -3,20 +3,34 @@ require 'deployml/project'
 namespace :deploy do
   task :project do
     @project = DeploYML::Project.new(Dir.pwd)
+
+    puts "Successfully loaded #{DeploYML::Project::CONFIG_FILE}"
   end
 
   desc 'Downloads the project'
   task :download => :project do
+    puts "Downloading project from #{@project.config.source} ..."
+
     @project.download!
+
+    puts "Project downloaded."
   end
 
   desc 'Uploads the project'
   task :upload => :project do
+    puts "Uploading project to #{@project.config.dest} ..."
+
     @project.upload!
+
+    puts "Project uploaded."
   end
 
   task :deploy => :project do
+    puts "Deploying project to #{@project.config.dest} ..."
+
     @project.deploy!
+
+    puts "Project deployed."
   end
 end
 

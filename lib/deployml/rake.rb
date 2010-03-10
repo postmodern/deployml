13396587e90ad9732eafeecda15f57cab8cae3eb
postmodern <postmodern.mod3@gmail.com>
2010-03-10 01:07:14 -0800

    Added a simple set of rake tasks to use DeploYML.

---
+++ lib/deployml/rake.rb
@@ -0,0 +1,1 @@
+require 'deployml/rake/tasks'
---
+++ lib/deployml/rake/tasks.rb
@@ -0,0 +1,21 @@
+require 'deployml/project'
+
+namespace :deploy do
+  task :project do
+    @project = DeploYML::Project.find
+  end
+
+  task :download => :project do
+    @project.download!
+  end
+
+  task :upload => :project do
+    @project.upload!
+  end
+
+  task :deploy => :project do
+    @project.deploy!
+  end
+end
+
+task :deploy => 'deploy:deploy'
